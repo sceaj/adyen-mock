52a53bead1717c1d4939faf3e1091c977f03ee57
sceaj <sceajr@gmail.com>
2024-12-21 08:17:39 -0700

    Beginnings of backend support for configuration

---
+++ src/main/java/sceaj/adyenmock/configuration/AdyenMockConfiguration.java
@@ -0,0 +1,12 @@
+package sceaj.adyenmock.configuration;
+
+import org.springframework.stereotype.Component;
+
+import sceaj.adyenmock.configuration.passthrough.PassthroughConfiguration;
+
+@Component
+public class AdyenMockConfiguration {
+	
+	private PassthroughConfiguration passthrough;
+
+}
